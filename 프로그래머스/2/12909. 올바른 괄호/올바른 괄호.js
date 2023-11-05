34bd5795674c5984ccb2289333359ee3a2376e73
Jang Moonseong <ericajg324@naver.com>
2023-11-05 12:28:35 +0900

    [level 2] Title: 올바른 괄호, Time: 2.40 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,8 +1,14 @@
 function solution(s){
     let count = 0;
     for (let i = 0; i < s.length; i++) {
-        count += s[i] === '(' ? 1 : -1;
-        if (count < 0) return false;
+        if( s[i] === '('){
+            count += 1
+        }else{
+             if (count === 0) return false;
+            count -= 1
+        }
+       // count += s[i] === '(' ? 1 : -1;
+       
     }
     return count === 0 ? true : false;
 }
