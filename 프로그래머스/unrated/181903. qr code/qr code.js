45c72444ee3a15db9c50b651d318aad4e27f40d6
Jang Moonseong <ericajg324@naver.com>
2023-11-18 19:33:23 +0900

    [level unrated] Title: qr code, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.js
+++ 프로그래머스/unrated/181903. qr code/qr code.js
@@ -1,6 +1,10 @@
+
 function solution(q, r, code) {
     const answer = []
-    for(let i = 0; i< code.length; i++)
-        if(i % q === r) answer.push(code[i])
+    for(let i = 0; i< code.length; i++){
+        if(i % q === r){
+            answer.push(code[i])
+        }
+    }
     return answer.join('')
 }
