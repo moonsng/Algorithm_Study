a5c642922919a7a04701b29fb45d2cadf522f620
Jang Moonseong <ericajg324@naver.com>
2023-12-17 09:22:38 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    answer = 0
+    odd = 0
+    even = 0
+    for i in range(len(num_list)):
+        if(i % 2 != 0):
+            odd += num_list[i]
+        else:
+            even += num_list[i]
+    if(odd >= even):
+        return odd
+    else:
+        return even
